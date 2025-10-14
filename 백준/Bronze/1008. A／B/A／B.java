3aa59a78818caa7b39de24bb06c50462426092d8
OHJOUYEON <91864044+OH-JOU-YEON@users.noreply.github.com>
2025-10-14 19:19:46 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 16128 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        double a = Double.parseDouble(st.nextToken());
+         double b  = Double.parseDouble(st.nextToken());
+
+      
+
+     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+		bw.write((a/b) + "\n");
+		
+ 
+		bw.flush();
+		bw.close();
+       
+
+    }
+
+}
